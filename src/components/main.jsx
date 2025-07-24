980bea5ab3fe6f9d10900dcf7a5114fa4ef1adce
Vineet <vineetkumar12392@gmail.com>
2025-07-25 04:00:29 +0530

    Fix background image path for GitHub Pages deployment

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -7,7 +7,7 @@ const Home = () => {
         <div className="card bg-dark text-white border-0 mx-3">
           <img
             className="card-img img-fluid"
-            src="./assets/main.png.jpg"
+            src={process.env.PUBLIC_URL + "/assets/main.png.jpg"}
             alt="Card"
             height={500}
           />
